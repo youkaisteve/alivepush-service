fdd6213261953ec855f094e5ba7931e4d256982e
youkai <youkaisteve@gmail.com>
2017-09-19 11:20:31 +0800

    config refresh

--- auth-service/src/main/java/cn/sm/alivepush/auth/controller/AuthController.java
+++ auth-service/src/main/java/cn/sm/alivepush/auth/controller/AuthController.java
@@ -3,11 +3,14 @@ package cn.sm.alivepush.auth.controller;
 import cn.sm.alivepush.contract.entity.ApplicationUser;
 import cn.sm.alivepush.contract.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by youkai on 2017/9/18.
@@ -21,8 +24,18 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @Value("${profile}")
+    private String profile;
+
     public AuthController(BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @GetMapping("/info")
+    public HashMap serviceInfo() {
+        HashMap map = new HashMap();
+        map.put("profile", profile);
+        return map;
     }
 
     @PostMapping("/sign-up")
@@ -30,6 +43,7 @@ public class AuthController {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userService.save(user);
     }
+
     @GetMapping("/list")
     public ArrayList<ApplicationUser> getUserList() {
         return userService.getUserList();
